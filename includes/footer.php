ba28481b8bb3d4f8d88ca0688abb510cc6d4aa25
milindx <127479615+milindx@users.noreply.github.com>
2023-08-17 22:14:31 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php echo date('Y');?>  Library Management System |<a href="" target="_blank"  > Designed by : Milind</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
